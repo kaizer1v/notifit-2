a0ec90e437387b886fa5a0169020dc76ebb8991e
kaizer1v <kaizer1v@gmail.com>
2018-10-23 13:44:47 +0530

    structure of invocation

--- dist/new-notifit.js
+++ dist/new-notifit.js
@@ -1,4 +1,4 @@
-;(function(global) {
+;(function(global, document) {
 
   function Notif(options) {
     return new Notif.init(options)
@@ -8,10 +8,32 @@
     type: 'default',
     position: 'right',
     msg: 'Huston, we\'ve got a problem.',
+    selector: 'body',
     opacity: 1,
     zindex: null,
     callback: null,
     clickable: false
+  }
+
+  var id = 'notifit-' + Date.now()
+
+  var elem = {}
+
+  function construct(config) {
+    // get the selector provided as a container
+    var container = document.querySelector(config.selector)
+    // create a `div`
+    var div = document.createElement('div')
+    div.id = id
+    // create a `p`
+    var p = document.createElement('p')
+    p.innerHTML = config.msg
+    // put the `p` inside the `div`
+    div.appendChild(p)
+    // finally append it to the selector
+    container.appendChild(div)
+    // assign it to the private var to refer to it later
+    elem = div
   }
 
   Notif.prototype = {
@@ -19,21 +41,25 @@
     constructor: Notif,
 
     // ... prototype methods
-    construct: function() {
-      var id = 'notifit-' + Date.now()
-      var div = document.createElement('div')
-      div.id = id
+    setStyle: function() {
+      console.log(elem)
+      debugger
+      return this
     }
   }
 
   Notif.init = function(options) {
-    // ... constructor function
     var self = this
-    var config = options || defaults
+
+    // merge the provided options with defaults
+    var config = Object.assign(defaults, options)
+
+    // construct the element
+    construct(config)
   }
 
   Notif.init.prototype = Notif.prototype
 
   // finally expose the `Notif` function globally
   global.Notif = Notif
-}(window))
+}(window, document))
